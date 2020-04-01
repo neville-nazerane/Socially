2dff91a4ad0643a6bab887c3cfdc09479bdc97c9
Neville <live.neville@live.com>
2020-04-01 00:35:09 -0700

    db initialized

---
+++ src/Socially.WebAPI/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Socially.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Socially.WebAPI.Controllers
+{
+
+    [ApiController]
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<User> _signInManager;
+
+        public AccountController(SignInManager<User> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+    }
+}
