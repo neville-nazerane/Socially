ccbf8666c731bfceb46a75d4e36a1304d38d84bc
Neville <live.neville@live.com>
2020-04-01 19:08:34 -0700

    dotnet  5

---
+++ src/Socially.Server.Managers/UserVerificationManager.cs
@@ -0,0 +1,25 @@
+﻿using Socially.Server.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Socially.Server.Managers
+{
+    public class UserVerificationManager
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public UserVerificationManager(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<bool> EmailExistsAsync(string email, CancellationToken cancellationToken = default)
+        {
+            await 
+        }
+
+    }
+}
