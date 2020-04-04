03aafcebc41d067246ba509b4ebdc5551272ccdd
Neville <live.neville@live.com>
2020-04-04 14:41:41 -0700

    updated working intigration tests

---
+++ src/Socially.WebAPI.IntegrationTests/AccountTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using Socially.Core.Models;
+using System.Net.Http.Json;
+
+namespace Socially.WebAPI.IntegrationTests
+{
+    public class AccountTests : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        private readonly CustomWebApplicationFactory<Startup> _factory;
+
+        public AccountTests(CustomWebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task SignupAndLogin()
+        {
+            var client = _factory.CreateClient();
+
+            var signupModel = new SignUpModel
+            {
+                Email = "ya@goo.com",
+                UserName = "username",
+                Password = "pasSword!2",
+                ConfirmPassword = "pasSword!2"
+            };
+
+            var signinRes = await client.PostAsJsonAsync("signup", signupModel);
+            Assert.True(signinRes.IsSuccessStatusCode,
+                                $"Sign in had error code {signinRes.StatusCode} saying '{await signinRes.Content.ReadAsStringAsync()}'");
+
+
+        }
+
+    }
+
+}
---
+++ src/Socially.WebAPI.IntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Socially.Server.DataAccess;
+
+public class CustomWebApplicationFactory<TStartup>
+    : WebApplicationFactory<TStartup> where TStartup : class
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+
+        builder.ConfigureServices(services =>
+        {
+            // Remove the app's ApplicationDbContext registration.
+            var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<ApplicationDbContext>));
+
+            if (descriptor != null)
+            {
+                services.Remove(descriptor);
+            }
+
+            // Add ApplicationDbContext using an in-memory database for testing.
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseInMemoryDatabase("InMemoryDbForTesting");
+            });
+
+        });
+    }
+}
