596f5b030ee85bdc5c078d320b3363f638ac4c33
Neville <live.neville@live.com>
2020-04-19 19:42:03 -0700

    merged consumers

---
+++ src/Socially.MobileApps.Services/ApiConsumer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace Socially.MobileApps.Services
+{
+    public class ApiConsumer
+    {
+        private readonly HttpClient _client;
+
+        private const string accountPath = "api/account";
+
+        public ApiConsumer(HttpClient client)
+        {
+            _client = client;
+        }
+
+        
+
+    }
+}
