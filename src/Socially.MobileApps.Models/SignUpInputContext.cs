23d16f818639534a126b32669666eefe2bd841f5
Neville <live.neville@live.com>
2020-04-20 01:48:21 -0700

    inputs setup

---
+++ src/Socially.MobileApps.Models/SignUpInputContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Socially.MobileApps.Models
+{
+    public class SignUpInputContext
+    {
+
+        public string Text { get; set; }
+
+        public bool IsPassword { get; set; }
+
+        public Command<SignUpInputContext> NextCommand { get; set; }
+
+        public string Title { get; set; }
+
+        public string Instructions { get; set; }
+
+        public string ErrorMessage { get; set; }
+
+    }
+}
---
+++ src/Socially.MobileApps.ViewModels/SignUpFlowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Socially.MobileApps.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace Socially.MobileApps.ViewModels
+{
+    public class SignUpFlowViewModel
+    {
+
+        public ObservableCollection<SignUpInputContext> Inputs { get; set; }
+
+        public SignUpFlowViewModel()
+        {
+            Inputs = new ObservableCollection<SignUpInputContext>();
+        }
+
+
+
+    }
+}
