2ae17cb76d120de336325b0ba0cda5076d6445b0
Neville <live.neville@live.com>
2020-04-21 01:17:04 -0700

    started creating a activity indicator for commands

---
+++ src/Socially.MobileApps/Socially.MobileApps/Components/ActivityIndicatorForCommand.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Socially.MobileApps.Components
+{
+    public class ActivityIndicatorForCommand : ActivityIndicator
+    {
+        public static readonly BindableProperty CommandToTrackProperty = BindableProperty.Create(nameof(CommandToTrack), typeof(Command), typeof(ActivityIndicatorForCommand));
+        public static readonly BindableProperty CommandParameterProperty = BindableProperty.Create(nameof(CommandParameter), typeof(object), typeof(ActivityIndicatorForCommand));
+
+
+        public Command CommandToTrack { get; set; }
+
+        public object CommandParameter { get; set; }
+
+        protected override void OnParentSet()
+        {
+            base.OnParentSet();
+            if (Parent is null)
+            {
+                if (CommandToTrack != null)
+                    CommandToTrack.CanExecuteChanged -= CommandToTrack_CanExecuteChanged;
+            }
+            else
+            {
+                if (CommandToTrack != null)
+                    CommandToTrack.CanExecuteChanged += CommandToTrack_CanExecuteChanged;
+            }
+        }
+
+        private void CommandToTrack_CanExecuteChanged(object sender, EventArgs e)
+        {
+            if (CommandToTrack is null) return;
+
+            UpdateBasedOnCanExecute();
+            // check for generic type
+        }
+
+        private void UpdateBasedOnCanExecute()
+        {
+            bool canExecute = CommandToTrack.CanExecute(CommandParameter);
+
+            IsVisible = canExecute;
+            IsEnabled = canExecute;
+            IsRunning = canExecute;
+        }
+
+    }
+}
