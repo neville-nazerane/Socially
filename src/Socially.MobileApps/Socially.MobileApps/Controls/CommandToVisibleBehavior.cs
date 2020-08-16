d9e853f30d97c000ebdec32efecab4b0cde4ea5b
Neville <live.neville@live.com>
2020-08-16 02:52:19 -0700

    new solution

--- src/Socially.MobileApps/Socially.MobileApps/Controls/CommandToVisibleBehavior.cs
+++ src/Socially.MobileApps/Socially.MobileApps/Controls/CommandToVisibleBehavior.cs
@@ -6,21 +6,13 @@ using Xamarin.Forms;
 
 namespace Socially.MobileApps.Controls
 {
-    public class CommandToVisibleBehavior : Behavior<View>
+    public class CommandToVisibleBehavior : BehaviorBase<View>
     {
-
-        private View _view;
-
 
         public static readonly BindableProperty CommandProperty = BindableProperty.Create(nameof(Command), 
                                                                                           typeof(ICommand), 
                                                                                           typeof(CommandToVisibleBehavior), 
-                                                                                          null,
-                                                                                          propertyChanged: delegate
-                                                                                          {
-
-                                                                                          });
-
+                                                                                          propertyChanged: CommandChanged);
 
 
         public static readonly BindableProperty CommandParameterProperty = BindableProperty.Create(nameof(CommandParameter),
@@ -28,26 +20,10 @@ namespace Socially.MobileApps.Controls
                                                                                         typeof(CommandToVisibleBehavior));
 
 
-
-        public static readonly BindableProperty OnlyStringProperty = BindableProperty.CreateAttached(nameof(OnlyString),
-                                                                                        typeof(string),
-                                                                                        typeof(CommandToVisibleBehavior),
-                                                                                        null,
-                                                                                        propertyChanged: (a, b, c) =>
-                                                                                        {
-
-                                                                                        });
-
-        public string OnlyString
-        {
-            get => (string)GetValue(OnlyStringProperty);
-            set => SetValue(OnlyStringProperty, value);
-        }
-
         public ICommand Command
         {
-            get => (ICommand)GetValue(CommandProperty); 
-            set => SetCommand(value); 
+            get => (ICommand)GetValue(CommandProperty);
+            set => SetValue(CommandProperty, value);
         }
 
         public object CommandParameter
@@ -56,34 +32,18 @@ namespace Socially.MobileApps.Controls
             set => SetValue(CommandParameterProperty, value);
         }
 
-        private void SetCommand(ICommand command)
+        private void SetCommand(ICommand oldCommand, ICommand newCommand)
         {
-            if (Command != null)
-                Command.CanExecuteChanged -= Command_CanExecuteChanged;
-            SetValue(CommandProperty, command);
-            if (Command != null)
-                Command.CanExecuteChanged += Command_CanExecuteChanged;
-        }
+            if (oldCommand != null)
+                oldCommand.CanExecuteChanged -= Command_CanExecuteChanged;
 
-        protected override void OnAttachedTo(View bindable)
-        {
-            base.OnAttachedTo(bindable);
-            _view = bindable;
-            BindingContext = _view.BindingContext;
-            var cmd = Command;
-            //UpdateShown(false);
-        }
-
-        protected override void OnDetachingFrom(View bindable)
-        {
-            base.OnDetachingFrom(bindable);
-            if (Command != null)
-                Command.CanExecuteChanged -= Command_CanExecuteChanged;
+            if (newCommand != null)
+                newCommand.CanExecuteChanged += Command_CanExecuteChanged;
         }
 
         private void Command_CanExecuteChanged(object sender, EventArgs e)
         {
-            if (_view != null)
+            if (View != null)
             {
                 bool canExecute = Command.CanExecute(CommandParameter); 
                 UpdateShown(!canExecute);
@@ -92,8 +52,8 @@ namespace Socially.MobileApps.Controls
 
         private void UpdateShown(bool isShown)
         {
-            _view.IsVisible = isShown;
-            if (_view is ActivityIndicator indicator)
+            View.IsVisible = isShown;
+            if (View is ActivityIndicator indicator)
             {
                 indicator.IsEnabled = isShown;
                 indicator.IsRunning = isShown;
@@ -102,7 +62,7 @@ namespace Socially.MobileApps.Controls
 
         private static void CommandChanged(BindableObject bindable, object oldValue, object newValue)
         {
-            ((CommandToVisibleBehavior)bindable).Command = (ICommand)newValue;
+            ((CommandToVisibleBehavior)bindable).SetCommand((ICommand)oldValue, (ICommand)newValue);
         }
 
     }
