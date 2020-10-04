f0d9ffbf90733b6aace0ffd198c4590ffd3b662f
Neville <live.neville@live.com>
2020-10-04 16:51:05 -0700

    app outside look clean up

--- src/Socially.MobileApps/Socially.MobileApps.Android/MainActivity.cs
+++ src/Socially.MobileApps/Socially.MobileApps.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace Socially.MobileApps.Droid
 {
-    [Activity(Label = "Socially.MobileApps", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Socially", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
