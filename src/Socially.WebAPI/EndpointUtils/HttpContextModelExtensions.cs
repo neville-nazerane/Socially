c831a389833c0e346c2f9681935df27064d2aedb
Neville <live.neville@live.com>
2020-12-11 01:25:51 -0700

    started validation

---
+++ src/Socially.WebAPI/EndpointUtils/HttpContextModelExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Socially.WebAPI.EndpointUtils
+{
+    public static class HttpContextModelExtensions
+    {
+
+        public static async Task<(bool isValid, TModel model)> TryValidateModelAsync<TModel>(this HttpContext context, ICollection<ValidationResult> validationResults)
+        {
+            var model = await JsonSerializer.DeserializeAsync<TModel>(context.Request.Body);
+            bool isValid = Validator.TryValidateObject(model, new ValidationContext(model), validationResults);
+            return (isValid, model);
+        }
+
+    }
+}
