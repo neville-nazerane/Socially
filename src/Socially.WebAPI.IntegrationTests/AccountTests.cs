6e1c4277122d03f58f9dc843d6f7d2b414076fb2
Neville Nazerane <live.neville@live.com>
2022-08-07 16:16:23 -0700

    broken

--- src/Socially.WebAPI.IntegrationTests/AccountTests.cs
+++ src/Socially.WebAPI.IntegrationTests/AccountTests.cs
@@ -28,7 +28,7 @@ namespace Socially.WebAPI.IntegrationTests
             const string testEmail = "ya@goo.com";
             const string testUsername = "username";
             const string testPassword = "pasSword!2";
-            const string loginSource = "tester";
+            const string loginSource = "testerfake";
 
             // attempt signin
             var loginModel = new LoginModel
