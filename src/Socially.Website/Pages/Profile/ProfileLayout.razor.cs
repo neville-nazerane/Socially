7b7bcd177b1f88fc60c082f26c017e1b03d91fb4
Neville Nazerane <live.neville@live.com>
2022-11-05 19:51:27 -0700

    created friend page

---
+++ src/Socially.Website/Pages/Profile/ProfileLayout.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using Socially.Models;
+using Socially.Website.Services;
+using System.Threading.Tasks;
+
+namespace Socially.Website.Pages.Profile
+{
+    public partial class ProfileLayout
+    {
+
+        [Inject]
+        public CachedContext CachedContext { get; set; }
+
+        ProfileUpdateModel profileInfo;
+
+
+        protected override async Task OnInitializedAsync()
+        {
+            profileInfo = await CachedContext.GetCurrentProfileInfoAsync();
+        }
+
+    }
+}
