de7b56bd765b2fc9e63371623569b6dc53952280
Neville Nazerane <live.neville@live.com>
2022-11-20 01:53:03 -0700

    post components clean up

---
+++ src/Socially.Website/Components/AddPostComponent.razor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using Socially.Apps.Consumer.Services;
+using Socially.Models;
+using Socially.Website.Services;
+using System.Threading.Tasks;
+
+namespace Socially.Website.Components
+{
+    public partial class AddPostComponent
+    {
+
+        [Inject]
+        public IApiConsumer Consumer { get; set; }
+
+        [Inject]
+        public CachedContext CachedContext { get; set; }
+
+        AddPostModel addPostModel = new();
+
+        async Task AddPostAsyc()
+        {
+            if (string.IsNullOrEmpty(addPostModel.Text)) return;
+
+            await Consumer.AddPostAsync(addPostModel);
+            addPostModel = new AddPostModel();
+            StateHasChanged();
+        }
+
+
+    }
+}
