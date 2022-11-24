ad3b12febf3cd3b7365e5c80b6463ca7b9d80e9d
Neville Nazerane <live.neville@live.com>
2022-11-24 03:19:37 -0700

    pending clean ups

--- src/Socially.Website/Pages/Profile/ProfileLayout.razor.cs
+++ src/Socially.Website/Pages/Profile/ProfileLayout.razor.cs
@@ -1,6 +1,12 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Configuration;
+using Socially.Apps.Consumer.Services;
 using Socially.Models;
+using Socially.Website.Components;
 using Socially.Website.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Socially.Website.Pages.Profile
@@ -11,13 +17,29 @@ namespace Socially.Website.Pages.Profile
         [Inject]
         public CachedContext CachedContext { get; set; }
 
+        [Inject]
+        public IApiConsumer Consumer { get; set; }
+
+        [Inject]
+        public IConfiguration Config { get; set; }
+
         UserSummaryModel profileInfo;
+        private IEnumerable<string> images;
+        private IEnumerable<UserSummaryModel> friends;
 
-
-        protected override async Task OnInitializedAsync()
+        protected override Task OnInitializedAsync()
         {
-            profileInfo = await CachedContext.GetCurrentProfileInfoAsync();
+            return RunAllAsync(
+               async () => profileInfo = await CachedContext.GetCurrentProfileInfoAsync(),
+               async () => images = await Consumer.GetAllImagesOfUserAsync(),
+               async () => friends = await Consumer.GetFriendsAsync()
+            );
+
+            
         }
+
+        Task RunAllAsync(params Func<Task>[] funcs)
+            => Task.WhenAll(funcs.Select(f => f()).ToArray());
 
     }
 }
