b703cb47f4583873bf9e7fb94d171ea604994dc4
Neville Nazerane <live.neville@live.com>
2022-11-29 19:35:21 -0700

    console app to deploy to blob storage

---
+++ src/Socially.Utils.SimpleAzCopy/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using Azure.Storage.Blobs;
+
+const int argsCount = 3;
+
+if (args.Length != argsCount)
+{
+    throw new IndexOutOfRangeException($"Wrong number of arguments passed in. Found {args.Length}");
+}
+
+string connString = args[0];
+string containerName = args[1];
+string srcPath = args[2];
+
+
+var blobClient = new BlobServiceClient(connString);
+var containerClient = blobClient.GetBlobContainerClient(containerName);
+
+var files = Directory.GetFiles(srcPath, "*.*", SearchOption.AllDirectories);
+
+foreach (var file in files)
+{
+    Console.WriteLine($"Uploading {file}");
+    var fileInfo = new FileInfo(file);
+    string fullName = fileInfo.FullName.Replace(srcPath, "");
+    await using var stream = fileInfo.OpenRead();
+    await containerClient.UploadBlobAsync(fullName, stream);
+}
+
