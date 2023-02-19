8be11ede71def4bb998dbb8fdadae2110a04f522
Neville Nazerane <live.neville@live.com>
2023-02-18 19:15:18 -0700

    comment patch ups

--- src/Socially.Mobile.Logic/ComponentModels/PostDisplayComponentModel.cs
+++ src/Socially.Mobile.Logic/ComponentModels/PostDisplayComponentModel.cs
@@ -1,4 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Socially.Apps.Consumer.Services;
 using Socially.Mobile.Logic.Models;
 using Socially.Mobile.Logic.Services;
 using Socially.Mobile.Logic.ViewModels;
@@ -7,27 +9,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Socially.Mobile.Logic.ComponentModels
 {
     public partial class PostDisplayComponentModel : ViewModelBase
     {
         private readonly ICachedContext _cachedContext;
-
+        private readonly IApiConsumer _apiConsumer;
+        
         [ObservableProperty]
         PostDisplayModel post;
 
         [ObservableProperty]
         UserSummaryModel user;
 
-        public PostDisplayComponentModel(ICachedContext cachedContext)
+        public PostDisplayComponentModel(ICachedContext cachedContext, IApiConsumer apiConsumer)
         {
             _cachedContext = cachedContext;
+            _apiConsumer = apiConsumer;
         }
 
         partial void OnPostChanging(PostDisplayModel value)
         {
             User = _cachedContext.GetUser(value.CreatorId);
+        }
+
+        [RelayCommand]
+        public async Task AddCommentAsync(string text, CancellationToken cancellationToken = default)
+        {
+            await _apiConsumer.AddCommentAsync(new()
+            {
+                Text = text,
+                ParentCommentId = null,
+                PostId = Post.Id
+            }, cancellationToken);
         }
 
     }
