4b7087d3b8448207ae5dfc7da5c5f586f61c8038
Neville Nazerane <live.neville@live.com>
2023-05-27 00:58:16 -0700

    table storage setup

---
+++ src/Socially.Server.DataAccess/TableAccess.cs
@@ -0,0 +1,53 @@
+﻿using Azure.Data.Tables;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Socially.Server.DataAccess
+{
+    public class TableAccess
+    {
+
+        private TableServiceClient _tableServiceClient;
+        private ConcurrentDictionary<string, TableClient> _clients;
+
+        public TableAccess(string connString)
+        {
+            _tableServiceClient = new TableServiceClient(connString);
+            _clients = new();
+        }
+
+        TableClient GetTableClient(string tableName) 
+            => _clients.GetOrAdd(tableName, t => _tableServiceClient.GetTableClient(tableName));
+
+        public Task AddAsync(string tableName, ITableEntity entity, CancellationToken cancellationToken = default)
+        {
+            TableClient tableClient = GetTableClient(tableName);
+            return tableClient.AddEntityAsync(entity, cancellationToken);
+        }
+
+        public IAsyncEnumerable<T> ListByPartitionAsync<T>(string tableName, 
+                                                           string partitionKey,
+                                                           int pageSize) 
+            where T : class, ITableEntity, new()
+        {
+            TableClient tableClient = GetTableClient(tableName);
+            var entities = tableClient.QueryAsync<T>(e => e.PartitionKey == partitionKey, maxPerPage: pageSize);
+            return entities;
+        }
+
+        public Task DeleteAsync(string tableName,
+                                string rowKey,
+                                string partitionKey,
+                                CancellationToken cancellationToken = default) 
+        {
+            TableClient tableClient = GetTableClient(tableName);
+            return tableClient.DeleteEntityAsync(partitionKey, rowKey, cancellationToken: cancellationToken);
+        }
+
+    }
+}
