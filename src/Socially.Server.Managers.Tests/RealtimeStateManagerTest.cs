11f731be63a9334076062c766ae6519ce3e70b00
Neville Nazerane <live.neville@live.com>
2023-05-27 04:04:01 -0700

    test base setup

---
+++ src/Socially.Server.Managers.Tests/RealtimeStateManagerTest.cs
@@ -0,0 +1,23 @@
+﻿using Moq;
+using Socially.Server.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Socially.Server.Managers.Tests
+{
+    public class RealtimeStateManagerTest : TestBase
+    {
+        private readonly Mock<ITableAccess> mockedTableAccess;
+        private readonly RealtimeStateManager manager;
+
+        public RealtimeStateManagerTest()
+        {
+            mockedTableAccess = new Mock<ITableAccess>();
+            manager = new RealtimeStateManager(mockedTableAccess.Object);
+        }
+
+    }
+}
