43fc4e4507a4556d916b09428b199868f2fb5af2
Neville Nazerane <live.neville@live.com>
2023-05-29 00:51:06 -0700

    updater

--- src/Socially.WebAPI/Hubs/DataUpdatesHub.cs
+++ src/Socially.WebAPI/Hubs/DataUpdatesHub.cs
@@ -1,5 +1,12 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.AspNetCore.SignalR;
 using Socially.Server.Managers;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Socially.WebAPI.Hubs
 {
@@ -11,6 +18,14 @@ namespace Socially.WebAPI.Hubs
         {
             _stateManager = stateManager;
         }
+
+        public Task ListenForPostsAsync(IEnumerable<int> ids, CancellationToken cancellationToken = default)
+        {
+            var tags = ids.Select(id => $"post_{id}").ToList();
+            return _stateManager.RegisterAsync(tags, Context.ConnectionId, cancellationToken);
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception) => _stateManager.UnregisterAsync(Context.ConnectionId);
 
     }
 }
